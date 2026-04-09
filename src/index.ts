4e6c2ce7cb0f79de9f07a8108ef5b34641ff813d
Guildford-George <georgeguildford1997@gmail.com>
2026-04-08 21:45:32 -1200

    feat: inital setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import morgan from "morgan"
+const app= express()
+
+const PORT= process.env.PORT || 3000
+
+app.use(morgan("dev"))
+// health check
+app.get("/", (req, res)=>{
+    res.send({
+        success: "ok",
+        message: "User Management Service"
+    })
+})
+
+app.listen(PORT, ()=>{console.log("User Management service running")})
